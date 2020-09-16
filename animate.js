c781fb96faf5d93ba62f72f8918e3f214f36c2db
al mamun <almamun00021@gmail.com>
2020-09-16 08:26:38 +0600

    second

---
+++ animate.js
@@ -0,0 +1,14 @@
+function animationMove() {
+  var elem = document.getElementById("animate");   
+  var pos = 0;
+  var id = setInterval(frame, 0);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++; 
+      elem.style.top = pos + "px"; 
+      elem.style.left = pos + "px"; 
+    }
+  }
+}
