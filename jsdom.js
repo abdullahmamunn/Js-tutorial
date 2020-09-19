72cfef7dcc8a4b23f744fda25c3c20187cfadfe5
al mamun <almamun00021@gmail.com>
2020-09-19 17:23:21 +0600

    Fourth

--- jsdom.js
+++ jsdom.js
@@ -45,3 +45,106 @@ function Myfunction()
 	var x = document.getElementById('name');
 	x.value = x.value.toUpperCase();
 }
+
+//objects
+var person = {
+    first_name : "Abdullah Al",
+    last_name  : "Mamun",
+    age        : 26,  
+	    fullname:function()
+	    {
+	    	return this.first_name +" "+this.last_name;
+	    }
+}
+console.log(person.fullname());
+document.getElementById('fullname').innerHTML = person.fullname();
+document.getElementById('age').innerHTML = "Age is "+person.age;
+
+//switch case
+
+
+function getMonth(){
+	var getNumber = Number(document.getElementById('getNumber').value);
+	//parseInt
+    var txt;
+
+	switch(getNumber)
+	{
+		case 1:
+			txt = "January";
+			break;
+		case 2:
+			txt = "February";
+			break;
+		case 3:
+			txt = "March";
+			break;
+		case 4:
+			txt = "April";
+			break;
+		case 5:
+			txt = "May";
+			break;
+		case 6:
+			txt = "June";
+			break;
+		case 7:
+			txt = "July";
+			break;
+		case 8:
+			txt = "August";
+			break;
+		case 9:
+			txt = "September";
+			break;
+		case 10:
+			txt = "October";
+			break;
+		case 11:
+			txt = "November";
+			break;
+		case 12:
+			txt = "December";
+			break;
+		default:
+			txt = "Invalid month";
+			break;
+	}
+  document.getElementById('getMonth').innerHTML = txt;
+
+
+	
+}
+  var text1 = document.getElementById('getText').innerText;
+  document.getElementById('show').innerHTML = text1;
+  console.log(text1);
+
+ 
+ var startTime;
+ var countDown;
+  function countDownInit() {	
+		
+		startTime = 20;
+        startTimeout();
+  }
+
+  function startTimeout()
+  {
+  	
+  	if(startTime>0)
+  	{
+  		startTime--;
+  		document.getElementById('show_count').innerHTML = startTime;
+  		if (startTime>0) 
+  		{
+           countDown = setTimeout("startTimeout()",1000);
+  		}
+
+  	}
+    
+  	
+  }
+function stopTimeout()
+{
+	clearTimeout(countDown);
+}
